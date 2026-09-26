62947878a80a35af88b3a27d4a4e0d432f59650c
Pulkit Sharma <pulkitsharmam2609@gmail.com>
2026-09-26 11:19:24 +0530

    Server now tracks number of clients being connected, and relays voice packets to everyone except sender

--- include/Network/server.hpp
+++ include/Network/server.hpp
@@ -9,9 +9,20 @@ class Server{
     private:
         void HandleMessage();
 
+        bool IsClientAlreadyConnected(
+            const boost::asio::ip::udp::endpoint& clientEndpoint
+        );
+        
+        void RelayVoicePacket(
+            const std::vector<std::uint8_t>& data,
+            const boost::asio::ip::udp::endpoint& sender
+        );
+
         boost::asio::io_context io_context;
         UdpSocket socket;
         unsigned short port;
+
+        std::vector<boost::asio::ip::udp::endpoint> clients;
 
     public:
         explicit Server(unsigned short port);
--- src/Network/server.cpp
+++ src/Network/server.cpp
@@ -26,8 +26,22 @@ void Server::HandleMessage(){
     }
 
     if(packet.GetType() == PacketType::Voice){
+        //if this is a new client, add it to the list
+
+        if(!IsClientAlreadyConnected(clientEndpoint)){
+            clients.push_back(clientEndpoint);
+            
+            std::cout<<"New Client Connected : "<<clientEndpoint.address().to_string()<<":"<<clientEndpoint.port()<<"\n";
+        }
+
         std::cout<<"Received Voice Packet : "<<packet.GetPayload().size()<<" bytes\n";
         //voice doesnt require any decoding because server doesnt care, it just acts as a mediator for transmission
+
+        //send this voice packet to every other client
+        RelayVoicePacket(
+            data,clientEndpoint
+        );
+
         return;
     }
 
@@ -57,4 +71,41 @@ void Server::Run(){
     while(true){
         HandleMessage(); //now the server continuously receives packets
     }
+}
+
+//check if the clients is aready present in the client list
+
+bool Server::IsClientAlreadyConnected(
+    const boost::asio::ip::udp::endpoint& clientEndpoint
+){
+    return std::find(
+        clients.begin(),
+        clients.end(),
+        clientEndpoint
+    ) != clients.end();
+}
+
+//this function sends the exact same voice packet to every client ecxept the sender
+
+void Server::RelayVoicePacket(
+    const std::vector<std::uint8_t> &data,
+    const boost::asio::ip::udp::endpoint& sender
+){
+    int clientsReached = 0;
+
+    for(int i=0; i<clients.size(); i++){
+        //we dont send packet back to the person who sent it 
+        if(clients[i] == sender){
+            continue;
+        }
+
+        socket.SendTo(
+            data,clients[i]
+        );
+
+        clientsReached++;
+        
+    }
+
+    std::cout<<"Relayed Voice Packet to : "<<clientsReached<<" clients\n";
 }
